79358937ec77533829422b2924b9b7e5293871fa
Minebox260 <39033036+Minebox260@users.noreply.github.com>
2025-01-26 01:41:51 +0100

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -26,7 +26,7 @@ export default {
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
-                <div class="type-title-sm">Verifier</div>
+                <div class="type-title-sm">First Victor</div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
